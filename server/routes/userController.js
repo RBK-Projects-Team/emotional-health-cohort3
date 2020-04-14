c7675c94fe305331b733ad960184f37726b4c7c8
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-04-14 01:15:21 +0100

    user controller

---
+++ server/routes/userController.js
@@ -0,0 +1,74 @@
+// Import users model
+User = require("../models/users.model");
+// Handle index actions
+exports.index = function (req, res) {
+  User.get(function (err, users) {
+    if (err) {
+      res.json({
+        status: "error",
+        message: err,
+      });
+    }
+    res.json({
+      status: "success",
+      message: "users retrieved successfully",
+      data: users,
+    });
+  });
+};
+// Handle create users actions
+exports.new = function (req, res) {
+  var user = new User();
+  user.user_name = req.body.user_name ? req.body.user_name : user.user_name;
+  user.password = req.body.password;
+  // save the users and check for errors
+  user.save(function (err) {
+    // if (err)
+    //     res.json(err);
+    res.json({
+      message: "New user created!",
+      data: user,
+    });
+  });
+};
+// Handle view user info
+exports.view = function (req, res) {
+  Users.findById(req.params.user_id, function (err, user) {
+    if (err) res.send(err);
+    res.json({
+      message: "user details loading..",
+      data: user,
+    });
+  });
+};
+// Handle update user info
+exports.update = function (req, res) {
+  User.findById(req.params.user_id, function (err, user) {
+    if (err) res.send(err);
+    user.user_name = req.body.user_name ? req.body.user_name : user.user_name;
+    user.password = req.body.password;
+    // save the user and check for errors
+    user.save(function (err) {
+      if (err) res.json(err);
+      res.json({
+        message: "User Info updated",
+        data: user,
+      });
+    });
+  });
+};
+// Handle delete user
+exports.delete = function (req, res) {
+  User.remove(
+    {
+      _id: req.params.user_id,
+    },
+    function (err, user) {
+      if (err) res.send(err);
+      res.json({
+        status: "success",
+        message: "User deleted",
+      });
+    }
+  );
+};
